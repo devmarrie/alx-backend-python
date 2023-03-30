4c5715e046aa42abfb25abd682a142865ec6b434
devmarrie <annemarrieakeyo42@gmail.com>
2023-03-31 00:37:24 +0300

    value union

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Takes a string k , int or float
+as an argument and returns a tuple
+Args:
+    - k: A string representing the key of the tuple.
+    - v: An integer or a float representing the value of the tuple.
+
+    Returns:
+    - A tuple containing the string k and the square of v as a float.
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int,float]) -> Tuple[str, float]:
+    """Return string k  and the square of v """
+    return (k, v**2)
