b85d17f8121c299bd630dfad2f60b8f35c3146ba
devmarrie <annemarrieakeyo42@gmail.com>
2023-04-03 23:51:11 +0300

    same implementation

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+from queue import PriorityQueue
+task_wait_random = __import__('3-tasks').task_wait_random
+"""Let's execute multiple coroutines at the same time with async
+"""
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with the specified max_delay.
+    """
+    delay = PriorityQueue()
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    for r in results:
+        delay.put(r)
+    return [delay.get() for _ in range(n)]
